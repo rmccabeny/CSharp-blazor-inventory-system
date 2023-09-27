3bb73291ab55572a4a1916f737902ac5c9fbf4cf
Robert McCabe <rmccabe1023@gmail.com>
2023-09-27 13:23:29 -0400

    Add project files.

---
+++ AceCorpIMS/Client/InventoryClient.cs
@@ -0,0 +1,83 @@
+﻿using AceCorpIMS.Client.Models;
+
+namespace AceCorpIMS.Client;
+
+public static class InventoryClient
+{
+    private static readonly List<Inventory> inventories = new()
+    {
+        new Inventory()
+        {
+            InventoryId = 1,
+            InventoryName = "Office Desk",
+            Quantity = 20,
+            Price = 74.99M,
+            DateReceived = new DateTime(2023, 10, 1)
+
+
+        },
+         new Inventory()
+        {
+            InventoryId = 2,
+            InventoryName = "Office Chair",
+            Quantity = 20,
+            Price = 49.99M,
+            DateReceived = new DateTime(2023, 10, 1)
+
+
+        },
+         new Inventory()
+        {
+            InventoryId = 3,
+            InventoryName = "Office Computer",
+            Quantity = 20,
+            Price = 299.99M,
+            DateReceived = new DateTime(2023, 10, 1)
+
+
+        },
+         new Inventory()
+        {
+            InventoryId = 4,
+            InventoryName = "Copier",
+            Quantity = 2,
+            Price = 199.99M,
+            DateReceived = new DateTime(2023, 10, 1)
+
+
+        }
+
+    };
+
+    public static Inventory[] GetInventories()
+    {
+        return inventories.ToArray();
+    }
+
+    public static void AddInventory(Inventory inventory)
+    {
+        inventory.InventoryId = inventories.Max(inventory => inventory.InventoryId) + 1;
+        inventories.Add(inventory);
+    }
+
+    public static Inventory GetInventory(int InventoryId)
+    {
+        return inventories.Find(inventory => inventory.InventoryId == InventoryId) ?? throw new Exception("Could not find item!");
+    }
+
+    public static void UpdateInventory(Inventory updatedInventory)
+    {
+        Inventory existingInventory = GetInventory(updatedInventory.InventoryId);
+        existingInventory.InventoryName = updatedInventory.InventoryName;
+        existingInventory.Quantity = updatedInventory.Quantity;
+        existingInventory.Price = updatedInventory.Price;
+        existingInventory.DateReceived= updatedInventory.DateReceived;
+    }
+
+    public static void DeleteInventory(int IventoryId)
+    {
+        Inventory inventory = GetInventory(IventoryId);
+        inventories.Remove(inventory);
+    }
+
+}
---
+++ AceCorpIMS/Client/Models/Inventory.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace AceCorpIMS.Client.Models;
+
+    public class Inventory
+    {
+    
+    public int InventoryId { get; set; }
+
+    [Required]
+    [StringLength(50)]
+    public required string InventoryName { get; set; }
+
+    [Range(1, 100)]
+    public int Quantity { get; set; }
+
+    [Range(1, 2000)]
+    public decimal Price { get; set; }
+
+    [Required]
+    public DateTime DateReceived { get; set; }
+}
